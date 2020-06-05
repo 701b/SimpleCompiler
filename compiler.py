885e1fd4997d556a14dd68153be8c941cbc1a82a
701b <701b.vn14a@gmail.com>
2020-06-06 02:34:43 +0900

    remove git bug

--- compiler.py
+++ compiler.py
@@ -22,7 +22,7 @@ class Compiler:
     compile 메서드를 통해 소스 파일의 코드를 타겟 코드로 컴파일한다.
     """
 
-    TAG = "compiler"
+    TAG = "Compiler"
 
     def __init__(self):
         self._scanner = Scanner()
@@ -39,11 +39,8 @@ class Compiler:
             self._scanner.scan(file_path)
         except FileNotFoundError:
             log(Compiler.TAG, f"다음의 주소에서 파일을 읽지 못했습니다 : {file_path}")
-<<<<<<< Updated upstream
-=======
-        except InvalidTokenError as e:
+        except InvalidCodeError as e:
             print(e)
->>>>>>> Stashed changes
 
 
 class Scanner:
@@ -57,7 +54,7 @@ class Scanner:
     REGULAR_EXPRESSION_OF_TOKEN = {'delimiter': '\(|\)|\{|\}|,|;',
                                    'keyword': '(IF)|(ELSE)|(THEN)|(WHILE)',
                                    'vtype': '(int)|(char)',
-                                   'operator': '=|>|\+|\*',
+                                   'operator': '=|==|>|\+|\*',
                                    'word': '([a-z]|[A-Z])+',
                                    'num': '[0-9]+'}
 
@@ -70,14 +67,14 @@ class Scanner:
 
         총 세 단계로 나뉘며, 그 단계는 다음과 같다.
             * source code가 작성된 파일의 내용을 불러오는 단계
-            * 불러온 내용을 전처리하는 단계
-            * 전처리된 내용을 토큰으로 나누어 저장하는 단계
+            * 불러온 source code를 토큰으로 나누어 저장하는 단계
 
         Args:
             file_path: 분석할 소스 코드가 작성된 파일의 주소
 
         Raises:
             FileNotFoundError: 인자로 받은 file_path로부터 파일을 읽지 못한 경우 발생한다.
+            InvalidCodeError: 소스 코드에 알 수 없는 기호가 있을 때 발생한다.
         """
 
         def read_source_code_from(file_path: str) -> None:
@@ -85,28 +82,24 @@ class Scanner:
             file_reader = FileReader(file_path)
             self._source_code = file_reader.read_all()
 
-        def preprocess() -> None:
-            """코드 내용 중 공백, 줄바꿈을 모두 제거한다."""
-            self._source_code = self._source_code.replace(' ', '')
-            self._source_code = self._source_code.replace('\n', '')
-
         def split_to_token() -> None:
             """준비된 코드로부터 토큰을 추출한다."""
             source_code_temp = self._source_code
 
             while source_code_temp != "":
+                is_match = False
+
                 for key, value in Scanner.REGULAR_EXPRESSION_OF_TOKEN.items():
                     pattern = re.compile(value)
                     result = pattern.match(source_code_temp)
 
                     if result is not None:
-                        print(f"{key}:{result.group()}")
+                        log(Scanner.TAG, f"{key}:{result.group()}")
                         self._tokens.append(f"{key}:{result.group()}")
                         source_code_temp = source_code_temp.replace(result.group(), "", 1)
+                        is_match = True
                         break
 
-<<<<<<< Updated upstream
-=======
                 if is_match is False:
                     # 미리 정해진 정규 표현식에 패턴 매칭이 되지 않았다면, 공백과 문자열인지 확인한다.
                     pattern = re.compile("\s|\n")
@@ -119,16 +112,11 @@ class Scanner:
                         pattern = re.compile("[^\s\n]*")
                         invalid_token = pattern.match(source_code_temp)
 
-                        raise InvalidTokenError(self._source_code, invalid_token.group())
+                        raise InvalidCodeError(self._source_code, invalid_token.group())
 
->>>>>>> Stashed changes
         log(Scanner.TAG, f"다음의 경로에서 소스 코드를 불러옵니다 : {file_path}")
         read_source_code_from(file_path)
         log(Scanner.TAG, f"읽어온 소스 코드::\n{self._source_code}\n")
-
-        log(Scanner.TAG, "코드를 분석하기 전에 전처리 과정을 수행합니다")
-        preprocess()
-        log(Scanner.TAG, f"전처리된 소스 코드::\n{self._source_code}\n")
 
         log(Scanner.TAG, "준비된 코드를 토큰으로 분리합니다.")
         split_to_token()
@@ -136,11 +124,9 @@ class Scanner:
 
     def get_token(self, index: int) -> str:
         return self._tokens[index]
-<<<<<<< Updated upstream
-=======
 
 
-class InvalidTokenError(Exception):
+class InvalidCodeError(Exception):
     """소스 코드에 알 수 없는 토큰이 있을 때 발생하는 에러
 
     에러 발생 시 해석할 수 없는 토큰을 출력하고, 소스 코드에서 문제의 토큰이 있는 줄을 출력한다.
@@ -176,4 +162,3 @@ class InvalidTokenError(Exception):
 compiler = Compiler()
 file_path = input()
 compiler.compile(file_path)
->>>>>>> Stashed changes
